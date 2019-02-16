e97eed0d5836eb11888078fb2ff32c17edb9e493
XProger <xproger@list.ru>
2019-02-16 06:48:18 +0300

    fix WebGL build

--- src/gapi_gl.h
+++ src/gapi_gl.h
@@ -712,8 +712,7 @@ namespace GAPI {
             Core::active.textures[0] = NULL;
             bind(0);
 
-
-            if (fmt == FMT_SHADOW) {
+            if (isShadow) {
                 glTexParameteri(target, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
                 glTexParameteri(target, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
             }
@@ -1316,7 +1315,7 @@ namespace GAPI {
                 #ifdef _OS_ANDROID
                     glInvalidateFramebuffer(GL_FRAMEBUFFER, count, discard);
                 #else
-                    glDiscardFramebuffer(GL_FRAMEBUFFER, count, discard);
+                    glDiscardFramebufferEXT(GL_FRAMEBUFFER, count, discard);
                 #endif
             }
         }
